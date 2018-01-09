3378c845f1a83007d506e31cec34c2f2aa3aed4d
Anwar Montasir <anwarmontasir@gmail.com>
2018-01-09 12:55:27 -0800

    updated 02-the-mobile-web for webpack

---
+++ 02-the-mobile-web/project-demo/webpack.config.js
@@ -0,0 +1,43 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: `./src/main.js`,
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/build`
+  },
+  plugins: [
+    new HtmlPlugin({ template: `./src/index.html` }),
+    new HtmlPlugin({ template: `./src/forms-matter.html`, filename: `forms-matter.html` }),
+    new CopyWebpackPlugin([
+      // in output use images folder
+      {from: 'src/images', to: 'images'}
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            interpolate: true,
+            attrs: false
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1, }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
+};
