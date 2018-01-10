62e833dc16b6b7115fc9506bf59f513defeb6b48
Anwar Montasir <anwarmontasir@gmail.com>
2018-01-10 11:52:43 -0800

    add webpack to demo 03

---
+++ 03-css-layout-techniques/project-demo/webpack.config.js
@@ -0,0 +1,44 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: `./src/main.js`,
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/build`
+  },
+  plugins: [
+    new HtmlPlugin({ template: `./src/index.html` }),
+    new HtmlPlugin({ template: `./src/second-page.html`, filename: `second-page.html` }),
+    new HtmlPlugin({ template: `./src/third-page.html`, filename: `third-page.html` }),
+    new CopyWebpackPlugin([
+      // in output use images folder
+      {from: 'src/images', to: 'images'}
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            interpolate: true,
+            attrs: false
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1, }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
+};
