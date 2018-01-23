0cbaf5ec9f993a4c3f5fca807015ebbb6828f01d
Anwar Montasir <anwarmontasir@gmail.com>
2018-01-23 13:24:44 -0800

    fixed final post

--- 12-web-type-and-color/post/src/components/app/App.js
+++ 12-web-type-and-color/post/src/components/app/App.js
@@ -12,11 +12,27 @@ const template = new Template(html);
 
 export default class App {
 
+  updateStyle(newClass, domTarget) {
+    const newClassValue = newClass.options[newClass.selectedIndex].value;
+    domTarget.className = newClassValue;
+  }
+
   render() {
     const dom = template.clone();
     dom.querySelector('header').appendChild(new Header().render());
     dom.querySelector('main').appendChild(new Content().render());
     dom.querySelector('footer').appendChild(new Footer().render());
+
+    this.typeScale = dom.getElementById('type-scale');
+    this.fontPair = dom.getElementById('font-pair');
+
+    dom.getElementById('type-scale-select').onchange = event => {
+      this.updateStyle(event.target, this.typeScale);
+    };
+
+    dom.getElementById('font-pair-select').onchange = () => {
+      this.updateStyle(event.target, this.fontPair);
+    };
     
     return dom;
   }
