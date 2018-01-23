d6ab0eb250c1fe285d32fc112212c498d0832a69
Anwar Montasir <anwarmontasir@gmail.com>
2018-01-23 00:30:39 -0800

    web-type-and-color demo

---
+++ 12-web-type-and-color/src/components/app/App.js
@@ -0,0 +1,37 @@
+import Template from '../Template';
+import html from './app.html';
+import './app.css';
+import Header from './Header';
+import './header.css';
+import './style.css';
+import Content from '../content/Content.js';
+
+const template = new Template(html);
+
+export default class App {
+  render() {
+    const dom = template.clone();
+    dom.querySelector('header').appendChild(new Header().render());
+    dom.querySelector('main').appendChild(new Content().render());
+    
+    this.typeScale = dom.getElementById('type-scale');
+    
+    dom.getElementById('type-scale-select').onchange = () => {
+      const elem = window.event.srcElement;
+      const elemValue = elem.options[elem.selectedIndex].value;
+      
+      this.typeScale.className = elemValue;
+    };
+
+    this.fontPair = dom.getElementById('font-pair');
+    
+    dom.getElementById('font-pair-select').onchange = () => {
+      const elem = window.event.srcElement;
+      const elemValue = elem.options[elem.selectedIndex].value;
+      
+      this.fontPair.className = elemValue;
+    };
+    
+    return dom;
+  }
+}
