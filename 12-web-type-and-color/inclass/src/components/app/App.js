dc0e046fed5cf917026430151c13d960f1780925
Anwar Montasir <anwarmontasir@gmail.com>
2018-01-24 09:42:48 -0800

    add inclass folder

--- 12-web-type-and-color/inclass/src/components/app/App.js
+++ 12-web-type-and-color/inclass/src/components/app/App.js
@@ -12,11 +12,26 @@ const template = new Template(html);
 
 export default class App {
 
+  updateStyle(evtTarget, domWrapper) {
+    const evtTargetValue = evtTarget.options[evtTarget.selectedIndex].value;
+    domWrapper.className = evtTargetValue;
+  }
+
   render() {
     const dom = template.clone();
     dom.querySelector('header').appendChild(new Header().render());
     dom.querySelector('main').appendChild(new Content().render());
     dom.querySelector('footer').appendChild(new Footer().render());
+
+    this.typeScale = dom.getElementById('type-scale');
+    this.fontPair = dom.getElementById('font-pair');
+
+    dom.getElementById('type-scale-select').onchange = event => {
+      this.updateStyle(event.target, this.typeScale);
+    };
+    dom.getElementById('font-pair-select').onchange = event => {
+      this.updateStyle(event.target, this.fontPair);
+    };
     
     return dom;
   }
