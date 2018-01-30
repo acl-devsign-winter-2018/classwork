44f9ae990a5cb36781a998948ac544fb40433646
Anwar Montasir <anwarmontasir@gmail.com>
2018-01-30 15:23:10 -0800

    cloudinary responsive image demo

--- 16-advanced-responsive-images/src/components/home/Home.js
+++ 16-advanced-responsive-images/src/components/home/Home.js
@@ -12,15 +12,30 @@ export default class Home {
     const dom = template.clone();
 
     const picture = new Picture({
+      // keep aspect ratios and breakpoints in DESCENDING ORDER,
+      // and have the same amount of both
       aspectRatios: ['3:1', '5:2', '2:1', '3:2', '1:1'],
       breakpoints: [2000, 1500, 1200, 900, 600],
+      /* https://cloudinary.com/documentation/image_transformations 
+      crop fill, auto gravity, auto quality are what I used here.
+      */
       options: 'c_fill,g_auto,q_auto',
       fileName: 'wheatfield-with-crows-vincent-van-gogh-1890_nb7qd7.jpg',
       alt: 'Vincent Van Gogh, Wheatfield With Crows, 1890'
     });
     const pictureDom = picture.render();
 
+    const blossoms = new Picture({
+      aspectRatios: ['16:9', '16:9', '2:1', '2:1'],
+      breakpoints: [1750, 1250, 750, 500],
+      options: 'c_fill,g_auto,q_auto',
+      fileName: 'almond-blossom-vincent-van-gogh-1890_ligjn3.jpg',
+      alt: 'Vincent Van Gogh, blossoms, 1888'
+    });
+    const blossomDom = blossoms.render();
+
     dom.querySelector('#splash').appendChild(pictureDom);
+    dom.querySelector('#blossoms').appendChild(blossomDom);
     return dom;
   }
 
--- 16-advanced-responsive-images/src/components/picture/Picture.js
+++ 16-advanced-responsive-images/src/components/picture/Picture.js
@@ -10,21 +10,25 @@ export default class Picture {
   }
 
   createPicture(cloudinaryObj) {
-    let pictureTxt = '';
-    for(let i = 0; i < cloudinaryObj.aspectRatios.length; i++) {
+    let pictureHTML = '';
+
+    for(let i=0; i < cloudinaryObj.aspectRatios.length; i++) {
       const imgOptions = `${cloudinaryObj.options},ar_${cloudinaryObj.aspectRatios[i]},w_${cloudinaryObj.breakpoints[i]}`;
       const imgURL = getURL(cloudinaryObj.fileName, imgOptions);
       
       const retinaOptions = `${cloudinaryObj.options},ar_${cloudinaryObj.aspectRatios[i]},w_${cloudinaryObj.breakpoints[i] * 2}`;
       const retinaURL = getURL(cloudinaryObj.fileName, retinaOptions);
-      
+
       if(i < cloudinaryObj.aspectRatios.length - 1) {
-        pictureTxt += `<source media="(min-width: ${cloudinaryObj.breakpoints[(i + 1)]}px)" srcset="${imgURL}, ${retinaURL} 2x">`;
+        // if this is NOT the last image in the array, output the <source> element
+        pictureHTML += `<source media="(min-width: ${cloudinaryObj.breakpoints[(i + 1)]}px)" srcset=${imgURL}, ${retinaURL} 2x">`;
       } else {
-        pictureTxt += `<img srcset="${imgURL}, ${retinaURL} 2x" alt="${cloudinaryObj.alt}">`;
+        // if this IS the last image, output the <img> element
+        pictureHTML += `<img srcset="${imgURL}, ${retinaURL} 2x" alt="${cloudinaryObj.alt}">`;
       }
     }
-    return pictureTxt;
+    console.log('pictureHTML', pictureHTML);
+    return pictureHTML;
   }
 
   render() {
