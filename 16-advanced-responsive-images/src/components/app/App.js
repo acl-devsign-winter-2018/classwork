4c493bcac9ba2011cd81107bb41eac149b508433
Anwar Montasir <anwarmontasir@gmail.com>
2018-01-30 13:21:12 -0800

    class prework

--- 16-advanced-responsive-images/src/components/app/App.js
+++ 16-advanced-responsive-images/src/components/app/App.js
@@ -5,46 +5,15 @@ import './app.css';
 import Header from './header/Header';
 
 import Home from '../home/Home.js';
-import Bio from '../bio/Bio.js';
-
-import { removeChildren } from '../dom';
 
 const template = new Template(html);
 
-const map = new Map();
-map.set('#bio', Bio);
-
 export default class App {
-
-  constructor() {
-    this.hashChange = () => this.setPage();
-    window.addEventListener('hashchange', this.hashChange);
-  }
-
-  setPage() {
-    const routes = window.location.hash.split('/');
-    const page = routes[0];
-    if(page === this.page) return;
-
-    if(this.pageComponent) this.pageComponent.unrender();
-    this.page = page;
-    const Component = map.get(this.page) || Home;
-    this.pageComponent = new Component();
-    removeChildren(this.main);
-    this.main.appendChild(this.pageComponent.render());
-  }
-
   render() {
     const dom = template.clone();
-
     dom.querySelector('header').appendChild(new Header().render());
-    this.main = dom.querySelector('main');
-    this.setPage();
+    dom.querySelector('main').appendChild(new Home().render());
 
     return dom;
-  }
-
-  unrender() {
-    window.removeEventListener('hashchange', this.hashChange);
   }
 }
