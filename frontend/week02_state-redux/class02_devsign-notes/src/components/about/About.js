a439680fca976d853f347ee4fb5507a6abe4dcf9
Anwar Montasir <anwarmontasir@gmail.com>
2018-02-27 15:19:00 -0800

    demo setup

---
+++ frontend/week02_state-redux/class02_devsign-notes/src/components/about/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import DocumentTitle from 'react-document-title';
+
+export default class About extends Component {
+  render() {
+    return (
+      <DocumentTitle title="Budget Tracker | About"> 
+        <h1>About page</h1>
+      </DocumentTitle>
+    );
+  }
+}
---
+++ frontend/week02_state-redux/class02_devsign-notes/src/components/app/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Header from './Header';
+import About from '../about/About';
+import Contact from '../contact/Contact';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="grid-wrap">
+          <Header/>
+          <main id="main-content" role="main">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Redirect to="/" />
+            </Switch>
+          </main>
+          <footer id="main-footer">
+            <small>&copy; 2018 this app.</small>
+          </footer>
+        </div>
+      </Router>
+    );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return <h1>Home page</h1>;
+  }
+}
