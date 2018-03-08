c3bec5136fbe6406fe328831b430b1536133190a
martypdx <marty@martypdx.com>
2018-03-08 13:25:22 -0800

    setup for class

---
+++ frontend/week03_error-load-3rdParty-components/class09_moar-react/README.md
@@ -0,0 +1,22 @@
+Moar React and Third Party Components
+===
+
+## Agenda
+
+* accessing the jest mock
+* `Promises`
+* `this.children`
+* `ref`
+* Use `map` to update in reducers
+* More on selectors
+  * kinds
+    * `state` selectors
+    * filters
+    * computation/aggregation
+  * `reselect` convenience methods
+* webpack proxy with two part path, `/albums/123`
+  * And what about ghpages?
+
+## Third Party Components
+
+* React Transition Group
---
+++ frontend/week03_error-load-3rdParty-components/class09_moar-react/src/store/index.js
@@ -0,0 +1,23 @@
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { loading, error } from '../components/app/reducers';
+import promiseMiddleware from './promiseMiddleware';
+
+const reducer = combineReducers({
+  loading,
+  error
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(
+      thunk,
+      promiseMiddleware
+    ) 
+  )
+);
+
+export default store;
